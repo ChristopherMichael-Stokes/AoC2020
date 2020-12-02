1f5e149f1c2bbfc18da5cff3a69778b87aef5bfc
Christopher Michael-Stokes <christophermichaelstokes@gmail.com>
2020-12-02 14:24:55 +0000

    Grammar

--- day02/solution.py
+++ day02/solution.py
@@ -2,24 +2,24 @@ import re
 
 
 def get_splits(line):
-        # e.g. input '1-3 a: abcde'
-        character = re.search('[a-z]',line).group(0)
-        policy, pword = re.split('[a-z]\:', line)
-        x, y = [int(i) for i in policy.split('-')]
-        return character, pword.strip(), x, y
+    # e.g. input '1-3 a: abcde'
+    character = re.search('[a-z]',line).group(0)
+    policy, pword = re.split('[a-z]\:', line)
+    x, y = [int(i) for i in policy.split('-')]
+    return character, pword.strip(), x, y
 
 
 def part1(data):
     # part 1 - with regex
-    # find occurances of the key character in pword string,
+    # find occurrences of the key character in pword string,
     # ensuring that it is in between the min / max amount set
     valids = 0
     for line in data:
         character, pword, o_min, o_max = get_splits(line)
-        occurances = len(re.findall(character, pword))
-        valid = occurances >= o_min and occurances <= o_max
+        occurrences = len(re.findall(character, pword))
+        valid = occurrences >= o_min and occurrences <= o_max
         valids += valid
-        # print(character,o_min,o_max,pword, occurances)
+        # print(character,o_min,o_max,pword, occurrences)
     return valids
 
 def part2(data):
